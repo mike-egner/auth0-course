74c3b40f5b1de23a97457e562562560e8def25ce
Mike Egner <egg-meister@hotmail.com>
2019-05-20 13:42:28 +0200

    completed basic app structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import HomePage from "./Home";
+import ProfilePage from "./Profile";
+import Nav from "./Nav";
 
 function App() {
-  return <div className="App" />;
+  return (
+    <>
+      <Nav />
+      <div className="body">
+        <Route path="/" exact component={HomePage} />
+        <Route path="/profile" component={ProfilePage} />
+      </div>
+    </>
+  );
 }
 
 export default App;
