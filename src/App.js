b6f67724fb1cb5a235baf04ca38bb3736ca7b00e
Mike Egner <egg-meister@hotmail.com>
2019-05-20 21:38:47 +0200

    created Auth class and login button

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import HomePage from "./Home";
 import ProfilePage from "./Profile";
 import Nav from "./Nav";
+import Auth from "././auth/Auth";
 
-function App() {
-  return (
-    <>
-      <Nav />
-      <div className="body">
-        <Route path="/" exact component={HomePage} />
-        <Route path="/profile" component={ProfilePage} />
-      </div>
-    </>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.auth = new Auth(this.props.history);
+  }
+
+  render() {
+    return (
+      <>
+        <Nav />
+        <div className="body">
+          <Route
+            path="/"
+            exact
+            render={props => <HomePage auth={this.auth} {...props} />}
+          />
+          <Route path="/profile" component={ProfilePage} />
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
