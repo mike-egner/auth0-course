d5b4aacbe4780bb4fbce07513f9e663b5b5185d9
Mike Egner <egg-meister@hotmail.com>
2019-06-07 21:19:58 +0200

    added AuthContext to eliminate prop drilling

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -1,30 +1,36 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import PropTypes from "prop-types";
+import AuthContext from "./AuthContext";
 
-function PrivateRoute({ component: Component, auth, scopes, ...rest }) {
+function PrivateRoute({ component: Component, scopes, ...rest }) {
   return (
-    <Route
-      {...rest}
-      render={props => {
-        if (!auth.isAuthenticated()) return auth.login();
-        if (scopes.length > 0 && !auth.userHasScopes(scopes)) {
-          return (
-            <h1>
-              Unauthorized - you need the following scope(s) to view this page:{" "}
-              {scopes.join(", ")}.
-            </h1>
-          );
-        }
-        return <Component auth={auth} {...props} />;
+    <AuthContext.Consumer>
+      {auth => {
+        return (
+          <Route
+            {...rest}
+            render={props => {
+              if (!auth.isAuthenticated()) return auth.login();
+              if (scopes.length > 0 && !auth.userHasScopes(scopes)) {
+                return (
+                  <h1>
+                    Unauthorized - you need the following scope(s) to view this
+                    page: {scopes.join(", ")}.
+                  </h1>
+                );
+              }
+              return <Component auth={auth} {...props} />;
+            }}
+          />
+        );
       }}
-    />
+    </AuthContext.Consumer>
   );
 }
 
 PrivateRoute.propTypes = {
   component: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired,
   scopes: PropTypes.array
 };
 
